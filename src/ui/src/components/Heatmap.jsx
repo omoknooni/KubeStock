2a1397edb628cc5d56ad9188a769695ea48b107a
omoknooni <hihm0609@gmail.com>
2025-04-17 20:40:32 +0900

    fix: ui - dynamic heatmap blockColor, symbolUrl

--- src/ui/src/components/Heatmap.jsx
+++ src/ui/src/components/Heatmap.jsx
@@ -1,10 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Box, Card, CardContent, Typography } from "@mui/material";
 
 const Heatmap = () => {
     const heatmapRef = useRef(null);
+    const [blockColor, setBlockColor] = useState("change 1h");
+
+    const fetchStatus = async () => {
+        try {
+            const response = await fetch(`/api/stocks/market-status`);
+            if (!response.ok) {
+                throw new Error(`API error! ${response.status} ${response.statusText}`);
+            }
+            const data = await response.json();
+            const statusMap = {
+                "open": "change|60",
+                "extended-hours": "premarket_change",
+                "after-hours": "postmarket_change",
+                "closed": "change|60",
+            };
+            setBlockColor(statusMap[data.blockColor]);
+        } catch (error) {
+            console.error("Error fetching status:", error);
+        }
+    }
 
     useEffect(() => {
+        // market status API call
+        fetchStatus();
+
         const heatmapWidget = document.createElement("script");
         heatmapWidget.src = "https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js";
         heatmapWidget.async = true;
@@ -15,8 +38,8 @@ const Heatmap = () => {
             grouping: "sector",
             locale: "kr",
             blockSize: "market_cap_basic",
-            blockColor: "change",
-            symbolUrl: "http://localhost:3000/stocks/symbol",    // Redirect to ${symbolUrl}/?tvwidgetsymbol={symbol}
+            blockColor: blockColor,
+            symbolUrl: `${window.location.origin}/stocks/symbol`,    // Redirect to ${symbolUrl}/?tvwidgetsymbol={symbol}
             hasTopBar: false,
             isDataSetEnabled: false,
             isZoomEnabled: true,
@@ -25,7 +48,7 @@ const Heatmap = () => {
 
         });
         heatmapRef.current.appendChild(heatmapWidget);
-    }, []);
+    }, [blockColor]);
 
     return (
         <Card sx={{ marginTop: 2, width: "100%", height: "100%" }}>
