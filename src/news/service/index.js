b0311715ee7afd5ee8fb0fb38d71d21de09c082a
omoknooni <hihm0609@gmail.com>
2025-04-30 12:18:38 +0900

    feat: news - endpoint for prometheus's metrics scraping

--- src/news/service/index.js
+++ src/news/service/index.js
@@ -1,6 +1,8 @@
 // index.js
 require('dotenv').config();
 const express = require('express');
+const client = require('prom-client');
+
 const newsRoutes = require('./routes/news');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
@@ -8,6 +10,29 @@ const { db, testConnection } = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Define Prometheus Metric
+client.collectDefaultMetrics();
+
+// HTTP 요청 지연 Metric 수집
+const httpRequestDurationMs = new client.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'HTTP 요청 지연 시간 (ms)',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [50, 100, 200, 300, 500, 1000],
+  });
+  
+// 모든 요청에 Metric 수집되도록 미들웨어
+app.use((req, res, next) => {
+    const endTimer = httpRequestDurationMs.startTimer({
+        method: req.method,
+        route: req.route ? req.route.path : req.path,  // 라우트 패턴으로 라벨링
+    });
+    res.on('finish', () => {
+        endTimer({ status_code: res.statusCode });
+    });
+    next();
+});
 
 // 미들웨어 설정
 app.use(express.json());
@@ -20,6 +45,12 @@ app.use('/api/news', newsRoutes);
 
 app.get('/health', (req, res) => {
     res.send('Welcome to the News Service');
+});
+
+// Prometheus Metric Endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
 });
 
 // DB test connection
